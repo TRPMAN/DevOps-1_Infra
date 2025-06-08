3b8e9804d3e906a14412d7e8f2716ec1fab33393
TRPMAN <trp.man08@gmail.com>
2025-06-09 01:06:45 +0700

    Update VPC

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -3,7 +3,6 @@ module "vpc" {
   version = "5.1.2"
 
   name = "eks-vpc"
-
   cidr = "172.20.0.0/16"
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 
@@ -11,17 +10,16 @@ module "vpc" {
   public_subnets  = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"]
 
   enable_nat_gateway   = true
-  # it should create 3 nat gateway but we reduce the cost
-  single_nat_gateway   = true
+  single_nat_gateway   = true # reduce cost (should be 3 by default)
   enable_dns_hostnames = true
 
   public_subnet_tags = {
-    "kubernetes.io/cluster/${var.clusterName}" = "shared"
-    "kubernetes.io/role/elb"                   = 1
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
   }
 
   private_subnet_tags = {
-    "kubernetes.io/cluster/${var.clusterName}" = "shared"
-    "kubernetes.io/role/internal-elb"          = 1
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
   }
 }
